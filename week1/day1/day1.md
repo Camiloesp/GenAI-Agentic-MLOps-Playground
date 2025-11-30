0c66dc993c945498a058714595c708005dec083f
Camilo <camesp96@gmail.com>
2025-11-30 11:12:07 -0600

    Deploy first live AI app with OpenAI and vercel integration

--- week1/day1/day1.md
+++ week1/day1/day1.md
@@ -1,7 +1,12 @@
+Part 1:
+Quick app deployment with FastAPI and Vercel.
 (https://www.udemy.com/course/generative-and-agentic-ai-in-production/learn/lecture/52441427#questions)
 github: (https://github.com/ed-donner/production/blob/main/week1/day1.md)
 On day 1, I will deploy an AI on Vercel. (https://vercel.com)
 I'll use Cursor IDE.
 Warning: Make sure you tell the terminal that the root of the project folder is week# so vercen creates the versel folder files inside it.
 
+Part 2:
+Adding AI to the production app.
+I will use the OpenAI API for this.
 
--- week1/day1/instant.py
+++ week1/day1/instant.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from openai import OpenAI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def instant():
-    return "Live from production! :) second try"
+    client = OpenAI()
+    message= """
+    You are on a website that has just been deployed to production for the first time!
+    Please reply  with an enthusiastic message to welcome the user to the website, explaining that it is live on production for the first time!
+    """
+
+    messages = [{"role": "user", "content": message}]
+    response = client.chat.completions.create(model="gpt-5-nano", messages=messages)
+    reply = response.choices[0].message.content.replace("\n", "<br>")
+    html = f"<html><head><title>Live in an Instant!</title></head><body><p>{reply}</p></body></html>"
+
+    return html
 
 # Export the app for Vercel
 #__all__ = ["app"]
 
-
+# Change folder in terminal: cd .\week1\day1\
